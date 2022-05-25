c1019c1661d73bd659b54de8b0018be05037245c
ariannagreco3393 <a.greco3393@gmail.com>
2022-05-25 14:07:56 +0200

    initial commit add class user

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * # User class
+ * this class is for User
+ * @author Arianna Greco
+ * @copyright 2022 Arianna Greco
+ * @license MIT (aggiungere link alla licenza)
+ */
+class User {
+    public $firstname;
+    public $lastname;
+}
